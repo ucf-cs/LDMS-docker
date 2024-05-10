2b6bfd1375338b37cf5635b39e8d8cab2ac42bdf
Kenneth Lamar <Kenneth.Lamar@ucf.edu>
2024-05-10 17:42:37 -0400

    Update entrypoint to not start samplers or aggregators by default

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Start samplers and aggregator.
-source ldms_configs/setup.sh && bash ldms_configs/start-simple-sampler.sh && bash ldms_configs/start-simple-agg.sh
+source ldms_configs/setup.sh
+# bash ldms_configs/start-simple-sampler.sh
+# bash ldms_configs/start-simple-agg.sh
 
 # Start a bash shell.
 exec bash
